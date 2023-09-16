b697f9073bbbba7069c6530ec77da9b5998a295d
heislera <heislera@msoe.edu>
2023-09-15 20:04:29 -0500

    part 5: placing enemies throughout dungeon (enemy actions NOT implemented yet). EXTRA: updated UI so it looks like a traditional roguelike

--- tile_types.py
+++ tile_types.py
@@ -40,13 +40,25 @@ SHROUD = np.array((ord(" "), (255, 255, 255), (0, 0, 0)), dtype=graphic_dt)
 floor = new_tile(
     walkable=True,
     transparent=True,
-    dark=(ord(" "), (255, 255, 255), (50, 50, 150)),
-    light=(ord(" "), (255, 255, 255), (200, 180, 50)),
+
+        # UI design that follows the tutorial:
+    # dark=(ord(" "), (255, 255, 255), (50, 50, 150)),
+    # light=(ord(" "), (255, 255, 255), (200, 180, 50)),
+
+    # The UI code here follows a more traditional roguelike "feel":
+    dark=(ord("."), (100, 100, 100), (0, 0, 0)),
+    light=(ord("."), (200, 200, 200), (0, 0, 0)),
 )
 
 wall = new_tile(
     walkable=False,
     transparent=False,
-    dark=(ord(" "), (255, 255, 255), (0, 0, 100)),
-    light=(ord(" "), (255, 255, 255), (130, 110, 50)),
+
+        # UI design that follows the tutorial:
+    # dark=(ord(" "), (255, 255, 255), (0, 0, 100)),
+    # light=(ord(" "), (255, 255, 255), (130, 110, 50)),
+
+    # The UI code here follows a more traditional roguelike "feel":
+    dark=(ord("#"), (100, 100, 100), (0, 0, 0)),
+    light=(ord("#"), (200, 200, 200), (0, 0, 0)),
 )
